7cb4ca3a2865931fe468cd53c905c32beb7da14c
Alexey <ezhische@gmail.com>
2023-03-07 01:06:19 +0300

    Remove VM Created by NewVN* script

---
+++ Remove-FullVM.ps1
@@ -0,0 +1,35 @@
+﻿param(
+    [ValidateScript({
+        $existingVm = Get-VM -Name $_ -ErrorAction SilentlyContinue
+        if ($existingVm) {
+            return $True
+        }
+        throw "There no VM named '$_' in this server."
+        
+    })]
+    [Parameter(Mandatory=$true)]
+    [string]$VMName
+)
+$VM=Get-VM $VMName
+$VHDPath = Get-VHD -VMId $VM.VMId
+
+if ($VM.State -eq "Running") {
+    Write-Host "Stoping VM..."
+    Stop-VM -Name $VMName -Confirm -Force
+}
+if ($VM.IsClustered) {
+    Remove-ClusterGroup -VMId $VM.VMId -RemoveResources
+    Remove-VM -Name $VMName
+}
+else{
+    Remove-VM -Name $VMName
+}
+#Check if VM Deleted
+$existingVm = Get-VM -Name $VMName -ErrorAction SilentlyContinue
+$confPath =  Join-Path $VM.Path "$VMName"
+if (-not $existingVm){
+    $VHDPath|ForEach-Object {
+        Remove-Item $_.Path
+    }
+    Remove-Item -Path $VM.Path -Force
+}
